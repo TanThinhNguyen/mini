027feef70e523f4b051f1a58192056c987f3532e
Zang <bt.hoanggiang@gmail.com>
2024-03-28 05:29:47 +0700

    refactor: config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseConfigModule } from './database-config/database-config.module';
-import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [DatabaseConfigModule, UserModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres', //doi cai nay thanh ten postgres tuong ung
+      password: 'jnhbgvfc', //doi password tuong ung
+      database: 'api', //tao db ten api trong postgres
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
